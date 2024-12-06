cd521b2f4caa72d1263a8d1d4da5155cdb7f7aed
Paulo Vitor <pvitorv@gmail.com>
2024-12-05 23:44:19 -0300

    segunda fase

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //users
+    Route::get('/users-index', [UserController::class, 'index'])->name('user.index');
 });
 
 require __DIR__.'/auth.php';
+
